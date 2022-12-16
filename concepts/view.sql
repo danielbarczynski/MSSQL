2f7153008cffdb7557c191e237bdaf38f8fbefb7
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-12-16 20:39:50 +0100

    up

--- concepts/view.sql
+++ concepts/view.sql
@@ -1,6 +1,7 @@
 --  View is a virtual table based on the result-set of an SQL statement.
 -- A view contains rows and columns, just like a real table. The fields in a view are fields from one or more real tables in the database.
 -- You can add SQL statements and functions to a view and present the data as if the data were coming from one single table.
+-- n brief, a programmer cannot create views without using tables.
 create view SimpleView
 as
     select *
@@ -10,3 +11,16 @@ as
 go
 select *
 from SimpleView;
+
+create table test (tname varchar (30))
+insert into test values ('hey'), ('bye')
+
+go
+create view testview as
+select * from test 
+
+go
+drop table test
+
+-- it wont work when the table is deleted - it just a data holder
+select * from testview
