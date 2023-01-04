8f32d486dac9a280bb6916a33222485323f777a7
Daniel PC <daniel.barczynski@onet.pl>
2023-01-04 20:54:09 +0100

    up

--- concepts/having.sql
+++ concepts/having.sql
@@ -13,3 +13,14 @@ from Pokoje
     left join RodzajPokoju on RodzajPokoju.ID = Pokoje.RodzajPokojID
 group by Nazwa
 having count(*) > 2
+
+--------------------------------------
+
+select avg(Age) as c from Persons
+where Age > 20
+having avg(Age) > 30
+
+select FirstName, avg(Age) as c from Persons
+where Age > 20
+group by FirstName -- with FirstName must be group by
+having avg(Age) > 30
--- concepts/view.sql
+++ concepts/view.sql
@@ -2,7 +2,7 @@
 -- A view contains rows and columns, just like a real table. The fields in a view are fields from one or more real tables in the database.
 -- You can add SQL statements and functions to a view and present the data as if the data were coming from one single table.
 -- n brief, a programmer cannot create views without using tables.
-create view SimpleView
+alter view SimpleView
 as
     select *
     from Persons
